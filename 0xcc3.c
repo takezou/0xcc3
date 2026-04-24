f632e801bb942bc8edceb3aac1894fb0b6dbe8f0
Takeshi Fujiwara <156400+takezou@users.noreply.github.com>
2026-04-25 06:41:52 +0900

    Adding addition and subtraction features.

--- 0xcc3.c
+++ 0xcc3.c
@@ -8,10 +8,31 @@ int main(int argc, char **argv) {
     return 1;
   }
 
+  char *p = argv[1];
+ 
   printf(".intel_syntax noprefix\n");
   printf(".globl main\n");
   printf("main:\n");
-  printf("  mov rax, %d\n", atoi(argv[1]));
+  printf("  mov rax, %d\n", strtol(p, &p, 10));
+
+  while (*p) {
+    if (*p == '+') {
+      p++;
+      printf("  add rax, %d\n", strtol(p, &p, 10));
+      continue;
+    }
+
+    if (*p == '-') {
+      p++;
+      printf("  sub rax, %d\n", strtol(p, &p, 10));
+      continue;
+    }
+
+    fprintf(stderr, "unexpected character: '%c'\n", *p);
+    return 1;
+
+  }  
+
   printf("  ret\n");
   return 0;
 }
